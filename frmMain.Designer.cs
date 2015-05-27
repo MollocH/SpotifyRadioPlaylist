ba75de95ebd0e4a085e42438c035481ab5eaf0fe
MollocH <sebastien.terodde@googlemail.com>
2015-05-27 12:20:35 +0200

    Initial commit

---
+++ frmMain.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace SpotifyRadioPlaylist
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(57, 8);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(515, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Playlist";
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6});
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(748, 368);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Track";
+            this.columnHeader4.Width = 296;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Artist";
+            this.columnHeader5.Width = 254;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "URI";
+            this.columnHeader6.Width = 188;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.button1);
+            this.splitContainer1.Panel1.Controls.Add(this.comboBox1);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.listView1);
+            this.splitContainer1.Size = new System.Drawing.Size(748, 412);
+            this.splitContainer1.SplitterDistance = 40;
+            this.splitContainer1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(578, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 27);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(748, 412);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "frmMain";
+            this.Text = "SpotifyRadioPlaylist";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.frmMain_Closed);
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ frmMain.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SpotifyAPI.SpotifyWebAPI;
+using SpotifyAPI.SpotifyWebAPI.Models;
+using SpotifyAPI.SpotifyLocalAPI;
+using SpotifyEventHandler = SpotifyAPI.SpotifyLocalAPI.SpotifyEventHandler;
+
+namespace SpotifyRadioPlaylist
+{
+    public partial class frmMain : Form
+    {
+        SpotifyLocalAPIClass spotifyLocal;
+        SpotifyWebAPIClass spotifyWeb;
+        SpotifyMusicHandler mh;
+        SpotifyEventHandler eh;
+        ImplicitGrantAuth auth;
+
+        Boolean isAuth = false;
+        String playlist = null;
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            spotifyLocal = new SpotifyLocalAPIClass();
+
+            /*
+            if (!SpotifyLocalAPIClass.IsSpotifyWebHelperRunning())
+            {
+                DialogResult result = MessageBox.Show("Spotify Webhelper is not running. Should I start it for you ?", "Spotify Webhelper not running", MessageBoxButtons.YesNo);
+                if(result == DialogResult.Yes)
+                {
+                    spotifyLocal.RunSpotifyWebHelper();
+                    System.Threading.Thread.Sleep(1000);
+                }
+                else
+                {
+                    Environment.Exit(0);
+                }
+            }
+            */
+            
+            if (!SpotifyLocalAPIClass.IsSpotifyRunning())
+            {
+                DialogResult result = MessageBox.Show("Spotify is not running. Should I start it for you ?", "Spotify not running", MessageBoxButtons.YesNo);
+                if (result == DialogResult.Yes)
+                {
+                    spotifyLocal.RunSpotify();
+                    System.Threading.Thread.Sleep(1000);
+                }
+                else
+                {
+                    Environment.Exit(0);
+                }
+            }
+            
+            authentication();
+
+            while(isAuth == false)
+            {
+                System.Threading.Thread.Sleep(300);
+            }
+
+            mh = spotifyLocal.GetMusicHandler();
+            eh = spotifyLocal.GetEventHandler();
+            eh.OnTrackChange += new SpotifyEventHandler.TrackChangeEventHandler(trackchange);
+
+            populatePlaylist();
+        }   
+
+        private void authentication()
+        {
+            // Webauthentication
+            auth = new ImplicitGrantAuth()
+            {
+                //Your client Id
+                ClientId = "13d7960cb513461a85ee264b88b8f6b7",
+                //Set this to localhost if you want to use the built-in HTTP Server
+                RedirectUri = "http://localhost:43000",
+                //How many permissions we need?
+                Scope = Scope.USER_READ_PRIVATE | Scope.USER_READ_EMAIL | Scope.PLAYLIST_READ_PRIVATE | Scope.USER_LIBRARAY_READ | Scope.USER_LIBRARY_MODIFY | Scope.USER_READ_PRIVATE
+                        | Scope.USER_FOLLOW_MODIFY | Scope.USER_FOLLOW_READ | Scope.PLAYLIST_MODIFY_PRIVATE | Scope.USER_READ_BIRTHDATE | Scope.PLAYLIST_MODIFY_PUBLIC | Scope.PLAYLIST_MODIFY_PRIVATE
+            };
+
+            //Start the internal http server
+            auth.StartHttpServer(43000);
+            //When we got our response
+            auth.OnResponseReceivedEvent += auth_OnResponseReceivedEvent;
+            //Start
+            auth.DoAuth();
+            // Local Authentication
+        }
+
+        public void auth_OnResponseReceivedEvent(Token token, string state, string error)
+        {
+            //stop the http server
+            auth.StopHttpServer();
+
+            if (error != null)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Error: " + error);
+                //return;
+            }
+            spotifyWeb = new SpotifyWebAPIClass()
+            {
+                AccessToken = token.AccessToken,
+                TokenType = token.TokenType,
+                UseAuth = true
+            };
+            
+            spotifyLocal.Connect();
+
+
+
+            this.isAuth = true;
+        }
+
+        public void trackchange(TrackChangeEventArgs e)
+        {
+            this.Invoke((MethodInvoker)delegate {
+                updateList(e);
+            });
+            List<String> l = new List<String>();
+            l.Add(e.new_track.GetTrackURI());
+            
+            spotifyWeb.AddTracks(spotifyWeb.GetPublicProfile().Id, this.playlist, l); 
+        }
+
+        public void updateList(TrackChangeEventArgs e)
+        {
+            string[] row = { e.new_track.GetArtistName(), e.new_track.GetTrackURI()};
+            System.Diagnostics.Debug.WriteLine(e.new_track.GetTrackURI());
+            listView1.Items.Add(e.new_track.GetTrackName()).SubItems.AddRange(row);
+        }
+        
+        public void populatePlaylist()
+        {
+            comboBox1.Items.Clear();
+            Paging<SimplePlaylist> playlists = spotifyWeb.GetUserPlaylists(spotifyWeb.GetPublicProfile().Id);
+            DataTable dataTable = new DataTable("");
+            dataTable.Columns.Add("Value");
+            dataTable.Columns.Add("Name");
+            foreach (SimplePlaylist playlist in playlists.Items)
+            {
+                dataTable.Rows.Add(playlist.Id, playlist.Name);
+            }
+            comboBox1.DataSource = dataTable;
+            comboBox1.DisplayMember = "Name";
+            comboBox1.ValueMember = "Value";        
+        }  
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(button1.Text == "Start") { 
+                button1.Text = "Stop";
+                DataRow selectedDataRow = ((DataRowView)comboBox1.SelectedItem).Row;
+                this.playlist = selectedDataRow["Value"].ToString();
+                eh.ListenForEvents(true);
+            }
+            else
+            {
+                button1.Text = "Start";
+                eh.ListenForEvents(false);
+            }
+
+        }
+        private void frmMain_Closed(Object sender, FormClosedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+    }
+}
